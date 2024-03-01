7e479c870d574598f2900d1a8fa02561476a271e
Aldo Berziu <berziu2004@gmail.com>
2024-03-01 13:55:54 +0100

    new fix

--- pages/auth/[[...path]].tsx
+++ pages/auth/[[...path]].tsx
@@ -24,7 +24,7 @@ export default function Auth(): JSX.Element {
             <Head>
                 <title>SuperTokens 💫</title>
                 <link
-                    href={`https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap`}
+                    href={`//fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap`}
                     rel="stylesheet"
                     type="text/css"
                 />
