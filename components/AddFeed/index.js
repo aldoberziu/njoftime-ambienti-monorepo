76849a9a0e1b039fc9a439b3e6a892ac96c0a95a
Aldo Berziu <berziu2004@gmail.com>
2024-03-13 15:41:51 +0100

    metatag added on addfeed

--- components/AddFeed/index.js
+++ components/AddFeed/index.js
@@ -62,99 +62,111 @@ const AddFeed = () => {
   };
 
   return (
-    <div className={styles.container}>
-      <Text sh3>Publiko njoftimin me formularin me posht</Text>
-      <div className={`${styles.tile} ${styles.pattern1}`}>
-        <Dropdown
-          name="Kategoria"
-          field="category"
-          options={categories}
-          selectedValue={handleFeedInput}
-        />
-        <Input type="text" field="title" placeholder="Titulli" selectedValue={handleFeedInput} />
-        <Input
-          type="text"
-          field="description"
-          placeholder="Pershkrimi i njoftimit"
-          selectedValue={handleFeedInput}
-          className={`${styles.textarea} ${styles.lastChild}`}
-        />
-      </div>
-      <div className={`${styles.tile} ${styles.pattern2}`}>
-        <Dropdown
-          name="Struktura"
-          field="structure"
-          options={structures}
-          selectedValue={handleFeedInput}
-        />
-        <Input
-          type="number"
-          field="capacity"
-          placeholder="Kapaciteti"
-          selectedValue={handleFeedInput}
-        />
-        <Input type="number" field="floor" placeholder="Kati" selectedValue={handleFeedInput} />
-      </div>
-      <div className={`${styles.tile} ${styles.pattern3}`}>
-        <Input
-          type="number"
-          field="area"
-          placeholder="Siperfaqja neto m²"
-          selectedValue={handleFeedInput}
-        />
-        <Dropdown
-          name="Mobilimi"
-          field="furnishing"
-          options={furnishing}
-          selectedValue={handleFeedInput}
-        />
-        <Input type="number" field="toilet" placeholder="Tualete" selectedValue={handleFeedInput} />
-        <Dropdown
-          name="Ashensor"
-          field="elevator"
-          options={[
-            { _id: "true", title: "Po" },
-            { _id: "false", title: "Jo" },
-          ]}
-          selectedValue={handleFeedInput}
-        />
-      </div>
-      <div className={styles.uploadSection}>
-        <Text sh3>Vendosni Foto te ambientit*</Text>
-        <div className={styles.uploaders}>
-          <UploadImage selectedValue={handleFeedInput} field="images" index={0} />
-          <UploadImage selectedValue={handleFeedInput} field="images" index={1} />
-          <UploadImage selectedValue={handleFeedInput} field="images" index={2} />
-          <UploadImage selectedValue={handleFeedInput} field="images" index={3} />
+    <div>
+      <Head>
+        <title>SuperTokens 💫</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </Head>
+      <div className={styles.container}>
+        <Text sh3>Publiko njoftimin me formularin me poshte</Text>
+        <div className={`${styles.tile} ${styles.pattern1}`}>
+          <Dropdown
+            name="Kategoria"
+            field="category"
+            options={categories}
+            selectedValue={handleFeedInput}
+          />
+          <Input type="text" field="title" placeholder="Titulli" selectedValue={handleFeedInput} />
+          <Input
+            type="text"
+            field="description"
+            placeholder="Pershkrimi i njoftimit"
+            selectedValue={handleFeedInput}
+            className={`${styles.textarea} ${styles.lastChild}`}
+          />
         </div>
+        <div className={`${styles.tile} ${styles.pattern2}`}>
+          <Dropdown
+            name="Struktura"
+            field="structure"
+            options={structures}
+            selectedValue={handleFeedInput}
+          />
+          <Input
+            type="number"
+            field="capacity"
+            placeholder="Kapaciteti"
+            selectedValue={handleFeedInput}
+          />
+          <Input type="number" field="floor" placeholder="Kati" selectedValue={handleFeedInput} />
+        </div>
+        <div className={`${styles.tile} ${styles.pattern3}`}>
+          <Input
+            type="number"
+            field="area"
+            placeholder="Siperfaqja neto m²"
+            selectedValue={handleFeedInput}
+          />
+          <Dropdown
+            name="Mobilimi"
+            field="furnishing"
+            options={furnishing}
+            selectedValue={handleFeedInput}
+          />
+          <Input
+            type="number"
+            field="toilet"
+            placeholder="Tualete"
+            selectedValue={handleFeedInput}
+          />
+          <Dropdown
+            name="Ashensor"
+            field="elevator"
+            options={[
+              { _id: "true", title: "Po" },
+              { _id: "false", title: "Jo" },
+            ]}
+            selectedValue={handleFeedInput}
+          />
+        </div>
+        <div className={styles.uploadSection}>
+          <Text sh3>Vendosni Foto te ambientit*</Text>
+          <div className={styles.uploaders}>
+            <UploadImage selectedValue={handleFeedInput} field="images" index={0} />
+            <UploadImage selectedValue={handleFeedInput} field="images" index={1} />
+            <UploadImage selectedValue={handleFeedInput} field="images" index={2} />
+            <UploadImage selectedValue={handleFeedInput} field="images" index={3} />
+          </div>
+        </div>
+        <div className={`${styles.tile} ${styles.pattern1}`}>
+          <Dropdown
+            name="Qyteti"
+            field="location"
+            nested="city"
+            options={cities}
+            selectedValue={handleFeedInput}
+          />
+          <Dropdown
+            name="Zona"
+            field="location"
+            nested="zone"
+            options={zones.filter(({ cityId }) => cityId === feedInput?.location?.city)}
+            selectedValue={handleFeedInput}
+          />
+          <Input
+            type="text"
+            field="location"
+            nested="street"
+            placeholder="Shkruaj sakte emrin e rruges..."
+            selectedValue={handleFeedInput}
+            className={styles.lastChild}
+          />
+        </div>
+        <Button className={styles.publishButton} onClick={handleSubmit}>
+          Publiko
+        </Button>
       </div>
-      <div className={`${styles.tile} ${styles.pattern1}`}>
-        <Dropdown
-          name="Qyteti"
-          field="location"
-          nested="city"
-          options={cities}
-          selectedValue={handleFeedInput}
-        />
-        <Dropdown
-          name="Zona"
-          field="location"
-          nested="zone"
-          options={zones.filter(({ cityId }) => cityId === feedInput?.location?.city)}
-          selectedValue={handleFeedInput}
-        />
-        <Input
-          type="text"
-          field="location"
-          nested="street"
-          placeholder="Shkruaj sakte emrin e rruges..."
-          selectedValue={handleFeedInput}
-          className={styles.lastChild}
-        />
-      </div>
-      <Button className={styles.publishButton} onClick={handleSubmit}>
-        Publiko
-      </Button>
     </div>
   );
 };
