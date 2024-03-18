27607dc10f0c0511643b3a6c6c51aab27f4b5011
Aldo Berziu <berziu2004@gmail.com>
2024-03-18 14:47:44 +0100

    some url consolelogs

--- config/appInfo.ts
+++ config/appInfo.ts
@@ -1,12 +1,15 @@
 const apiBasePath = "/api/auth/";
 
 export const websiteDomain = process.env.REACT_APP_WEBSITE_URL || "http://localhost:3000";
+console.log({ websiteDomain });
 
 export function getApiDomain() {
+  console.log("api", process.env.REACT_APP_API_URL || "http://localhost:3001");
   return process.env.REACT_APP_API_URL || "http://localhost:3001";
 }
 
 export function getWebsiteDomain() {
+  console.log("website", process.env.REACT_APP_WEBSITE_URL || "http://localhost:3001");
   return process.env.REACT_APP_WEBSITE_URL || "http://localhost:3000";
 }
 
